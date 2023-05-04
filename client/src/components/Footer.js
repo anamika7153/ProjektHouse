be4c322631c0e1e6868e67f8d36104c18a1219a3
Anamika Yadav <aanyayadav419@gmail.com>
2023-05-05 00:21:55 +0530

    footer ui

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer style={{height: "25vh", bottom: "0 !important"}} className="page-footer">
           <div className="container">
             <div className="row">
-              <div className="col l6 s12">
+              <div className="col l6 s12 footer-left">
                 <h5 className="white-text">Contact Us</h5>
                 {/* <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p> */}
                 <ul>
@@ -15,7 +15,7 @@ const Footer = () => {
                     <li><a className="white-text" href="https://wa.me/+918601337075">WhatsApp</a></li>
                 </ul>
               </div>
-              <div className="col l4 offset-l2 s12">
+              <div className="col l4 offset-l2 s12 footer-right">
                 <h5 className="white-text">Created By</h5>
                 <ul>
                   {/* <li><a className="grey-text text-lighten-3" href="#!">Anamika Yadav</a></li> */}
