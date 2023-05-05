37d9477d84ca7c1db6b0a32991772f8247afef6a
Anamika Yadav <aanyayadav419@gmail.com>
2023-05-05 20:50:40 +0530

    external host with pass

--- server/middlewares/mailer.js
+++ server/middlewares/mailer.js
@@ -14,11 +14,11 @@ exports.mailer = async (mail,otp) => {
             auth: {
                 type: 'OAuth2',
                 user: process.env.MAIL_USERNAME,
-                // pass: process.env.MAIL_PASSWORD,
-                clientId: process.env.OAUTH_CLIENTID,
-                clientSecret: process.env.OAUTH_CLIENT_SECRET,
-                refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-                accessToken: accessToken
+                pass: process.env.MAIL_PASSWORD,
+                // clientId: process.env.OAUTH_CLIENTID,
+                // clientSecret: process.env.OAUTH_CLIENT_SECRET,
+                // refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+                // accessToken: accessToken
             }
         })
         
