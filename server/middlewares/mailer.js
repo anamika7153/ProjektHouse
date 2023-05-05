6e2d1c5267ff1ac23513eae18d79f4fa814194dc
Anamika Yadav <aanyayadav419@gmail.com>
2023-05-05 17:34:29 +0530

    nodemailer fix

--- server/middlewares/mailer.js
+++ server/middlewares/mailer.js
@@ -4,8 +4,8 @@ const { google } = require('googleapis')
 exports.mailer = async (mail,otp) => {
     const oAuth2Client = new google.auth.OAuth2(process.env.OAUTH_CLIENTID, process.env.OAUTH_CLIENT_SECRET, process.env.REDIRECT_URI)
     oAuth2Client.setCredentials({refresh_token: process.env.OAUTH_REFRESH_TOKEN})
-    const accessToken = await oAuth2Client.getAccessToken()
     try {
+        const accessToken = await oAuth2Client.getAccessToken()
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             // port: 465,
